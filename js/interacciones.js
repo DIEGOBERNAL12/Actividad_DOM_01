dda46e2c437c3be232521ea05af66faf647e77df
Diego Bernal E <benaldiegoa22@gmail.com>
2025-09-03 15:51:45 -0500

    se creo la segunda funcion que hace una estrella con el evento click

--- js/interacciones.js
+++ js/interacciones.js
@@ -5,8 +5,14 @@ function circulo(){
         figuras.style.backgroundColor = 'gray';
 }
 
+function estrella(){
+    figuras.style.clipPath = "polygon(50% 0%, 61% 35%, 98% 35%, 68% 57%, 79% 91%, 50% 70%, 21% 91%, 32% 57%, 2% 35%, 39% 35%)";
+    figuras.style.backgroundColor = "gold";
+}
+
 
 
 document.addEventListener('DOMContentLoaded', function(){
     document.getElementById('btnCircle').addEventListener('click', circulo);
+    document.getElementById('btnStar').addEventListener('click', estrella);
 })
